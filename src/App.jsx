432abb0b7de84bcf99afcf1b25372d1cbcd5f743
Josevan Oliveira <josivantarcio@gmail.com>
2025-05-12 20:33:56 -0300

    feat: refaz estrutura do projeto com todas as funcionalidades

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-screen bg-gray-50">
         <Header />
-        <main className="flex-grow">
+        <main className="flex-grow container mx-auto px-4 py-8">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<About />} />
+            <Route path="/contato" element={<Contact />} />
           </Routes>
         </main>
         <Footer />
