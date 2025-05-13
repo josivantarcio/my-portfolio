e27562ae0f90d5243c57c2cdc09d8cdb742c6a50
Josevan Oliveira <josivantarcio@gmail.com>
2025-05-13 10:51:55 -0300

    Fix routing configuration for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { I18nextProvider } from 'react-i18next'
-import i18n from './i18n'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -8,30 +6,29 @@ import About from './pages/About'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
 import Background from './components/Background'
+import { useTranslation } from 'react-i18next'
 
 function App() {
+  const { i18n } = useTranslation()
+
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng)
   }
 
   return (
-    <I18nextProvider i18n={i18n}>
-      <Router>
-        <div className="min-h-screen bg-black text-white">
-          <Background />
-          <Navbar onLanguageChange={changeLanguage} />
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </I18nextProvider>
+    <div className="min-h-screen bg-black text-white">
+      <Background />
+      <Navbar onLanguageChange={changeLanguage} />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   )
 }
 
